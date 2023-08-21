407acd8ae1f5e992c14be1530532bcf3520f87a1
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-08-21 15:25:49 +0600

    all is done except modal

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
+
 document.getElementById("firstCard").addEventListener("click", function(){
     const productName = document.getElementById("productName1").innerText;
-    setTextInTextArea(productName);
+    setTextInListContainer(productName);
+    const price = convertPriceStringIntoANumber("price1");
+    totalPriceAmount += price;
+    setTotalPrice(totalPriceAmount);
+    setTotal(totalPriceAmount);
 })
 document.getElementById("secondCard").addEventListener("click", function(){
     const productName = document.getElementById("productName2").innerText;
-    setTextInTextArea(productName);
+    setTextInListContainer(productName);
+    const price = convertPriceStringIntoANumber("price2");
+    totalPriceAmount += price;
+    setTotalPrice(totalPriceAmount);
+    setTotal(totalPriceAmount);
 })
 document.getElementById("thirdCard").addEventListener("click", function(){
     const productName = document.getElementById("productName3").innerText;
-    setTextInTextArea(productName);
+    setTextInListContainer(productName);
+    const price = convertPriceStringIntoANumber("price3");
+    totalPriceAmount += price;
+    setTotalPrice(totalPriceAmount);
+    setTotal(totalPriceAmount);
 })
 document.getElementById("fourthCard").addEventListener("click", function(){
     const productName = document.getElementById("productName4").innerText;
-    setTextInTextArea(productName);
+    setTextInListContainer(productName);
+    const price = convertPriceStringIntoANumber("price4");
+    totalPriceAmount += price;
+    setTotalPrice(totalPriceAmount);
+    setTotal(totalPriceAmount);
 })
 document.getElementById("fifthCard").addEventListener("click", function(){
     const productName = document.getElementById("productName5").innerText;
-    setTextInTextArea(productName);
+    setTextInListContainer(productName);
+    const price = convertPriceStringIntoANumber("price5");
+    totalPriceAmount += price;
+    setTotalPrice(totalPriceAmount);
+    setTotal(totalPriceAmount);
 })
 document.getElementById("sixthCard").addEventListener("click", function(){
     const productName = document.getElementById("productName6").innerText;
-    setTextInTextArea(productName);
+    setTextInListContainer(productName);
+    const price = convertPriceStringIntoANumber("price6");
+    totalPriceAmount += price;
+    setTotalPrice(totalPriceAmount);
+    setTotal(totalPriceAmount);
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,8 +1,39 @@
-function setTextInTextArea(productName){
+const totalPrice = document.getElementById("totalPriceAmount");
+const totalPriceString = totalPrice.innerText
+let totalPriceAmount = parseFloat(totalPriceString);
+function setTextInListContainer(productName){
     const listContainer = document.getElementById('listContainer');
     const p = document.createElement("p");
     p.classList.add("my-4")
     const count = listContainer.childElementCount;
     p.innerHTML = `${count + 1}. ${productName}`;
     listContainer.appendChild(p);
+}
+function convertPriceStringIntoANumber(priceId){
+    const priceString = document.getElementById(priceId).innerText;
+    const priceAmount = parseFloat(priceString);
+    return priceAmount;
+}
+function setTotalPrice (totalPriceAmount){
+    totalPrice.innerText = totalPriceAmount;
+    if (totalPriceAmount >= 200){
+        const applyButton = document.getElementById("applyButton");
+        applyButton.removeAttribute("disabled");
+        applyButton.addEventListener("click", function() {
+        const couponCode = document.getElementById("couponCode").value ;
+        if (couponCode === "SELL200"){
+            const discount = document.getElementById("discount");
+            const discountAmount = totalPriceAmount * 0.2;  //same as 20 percent
+            discount.innerText = discountAmount.toFixed(2);
+            const total = document.getElementById("total");
+            const totalAmount = totalPriceAmount - discountAmount;
+            total.innerText = totalAmount;
+        }
+        else {alert("sorry your coupon code is not correct ")}
+        })
+    }
+}
+function setTotal(totalPriceAmount){
+    const total = document.getElementById("total");
+    total.innerText = totalPriceAmount;
 }
